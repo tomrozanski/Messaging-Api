05af02664d364702bcc3d281aa72bf28e1b9b806
Tom <tomrozanski@gmail.com>
2021-05-22 04:54:11 +0300

    [Add] JWT Authentication

---
+++ auth/jwtAuth.py
@@ -0,0 +1,28 @@
+import jwt
+from app import *
+from datetime import datetime, timedelta
+
+
+def encode_auth_token(user_id):
+    try:
+        payload = {
+            'exp': datetime.utcnow() + timedelta(days=1),
+            'iat': datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+
+def decode_auth_token(auth_token):
+    try:
+        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'), algorithms=["HS256"])
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
